00cde3bb73e283d7a5a86c278b6c7c01fe4179f0
raku415 <raoul.kuratle@stud.hslu.ch>
2026-01-23 19:30:59 +0100

    Farbkorrektur und Requirements

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ st.markdown("""
         margin: 10px 0;
     }
     .warning-box {
-        background-color: #fff3e0;  /* Hellorange */
+        background-color: #DB942C;  /* Hellorange */
         padding: 15px;
         border-radius: 8px;
         border-left: 4px solid #ff9800;  /* Orange */
